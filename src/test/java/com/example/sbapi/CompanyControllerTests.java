1fcf05f0da566b6a74ab6076cbfabe2bd597f216
Brad Fontaine <bradf.83@gmail.com>
2019-05-20 14:56:34 -0600

    Add additional test.

--- src/test/java/com/example/sbapi/CompanyControllerTests.java
+++ src/test/java/com/example/sbapi/CompanyControllerTests.java
@@ -1,5 +1,7 @@
 package com.example.sbapi;
 
+import com.example.sbapi.model.Company;
+import com.example.sbapi.repository.CompanyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,6 +26,8 @@ public class CompanyControllerTests {
 
     @Autowired
     private MockMvc mvc;
+    @Autowired
+    private CompanyRepository companyRepository;
 
     @Test
     @WithMockUser
@@ -55,4 +61,19 @@ public class CompanyControllerTests {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$._embedded.companies", hasSize(1)));
     }
+
+    @Test
+    @WithMockUser
+    public void whenLookingUpASingleCompanyTheOwnerLinkIsPresent() throws Exception {
+        // Get the first company, look it up and then validate it has it's ownerLink and its the link that's expected.
+        Company company = companyRepository.findAll().iterator().next();
+        mvc.perform(get("/companies/" + company.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._links.ownerLink.*", hasSize(1)))
+                .andExpect(jsonPath("$._links.ownerLink.href", is("http://localhost/owners/" + company.getOwner().getId())))
+                .andDo(print());
+
+    }
+
 }
