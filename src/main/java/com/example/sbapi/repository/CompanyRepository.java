41ee97b46169e85baf5ec0319532bb3f26bbffad
Brad Fontaine <bradf.83@gmail.com>
2019-11-23 17:45:29 -0700

    Added a few interesting links for Query DSL.

--- src/main/java/com/example/sbapi/repository/CompanyRepository.java
+++ src/main/java/com/example/sbapi/repository/CompanyRepository.java
@@ -24,6 +24,26 @@ public interface CompanyRepository extends JpaRepository<Company, Long>,
     // Can pass parameter via Spring Data REST controller (....&time=2011-01-01T00:00:00Z)
     List<Company> findByCreatedAtBefore(Instant before);
 
+    /**
+     * Additional Information
+     *
+     * It seems you can also provide an @QueryDslPredicate to the Predicate parameter and customize the binding there,
+     * I was unable to get it to work in the repository.
+     *
+     * Links:
+     * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#core.web.type-safe
+     * https://www.programcreek.com/java-api-examples/index.php?api=org.springframework.data.querydsl.binding.QuerydslPredicate
+     * https://spring.io/blog/2015/09/04/what-s-new-in-spring-data-release-gosling#querydsl-web-support
+     * https://github.com/spring-projects/spring-data-examples/blob/querydsl-binding/rest/starbucks/src/main/java/example/stores/StoreRepository.java
+     * https://www.baeldung.com/rest-api-search-querydsl-web-in-spring-data-jpa
+     * https://stackoverflow.com/questions/48378013/how-can-you-use-different-operators-with-querydsl-web?rq=1
+     */
+
+    /**
+     * Customize the Query DSL binding so that all string properties are doing a like with ignore case
+     * @param bindings
+     * @param root
+     */
     @Override
     default public void customize(QuerydslBindings bindings, QCompany root) {
         // All strings are doing a 'like' ignoring case
